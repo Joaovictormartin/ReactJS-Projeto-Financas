ca5d5f4bbb2b6042558fbd10a4755ec453cfbe94
Joao <joao.teixeira@aedb.br>
2021-09-14 14:27:27 -0300

    adicionando o Modal no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { Header } from './components/Header';
-import { GlobalStyle } from './styles/global';
-import { Dashboard } from './components/Dashboard';
+import { useState } from "react";
 
-export function App(){
-  return(
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+export function App() {
+  const [isNewTransctionsModalOpen, setIsNewTransctionsModalOpen] = useState(false);
+
+  function handleOpenNewTransactionsModal() {
+    setIsNewTransctionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionsModal() {
+    setIsNewTransctionsModalOpen(false);
+  }
+
+  return (
     <>
-      <Header/>
-      <Dashboard/>
-      <GlobalStyle/>
+      <Header 
+        onOpenNewTransactionsModal={handleOpenNewTransactionsModal}
+      />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransctionsModalOpen}
+        onRequestClose={handleCloseNewTransactionsModal}
+      />
+
+      <GlobalStyle />
     </>
-  )
+  );
 }
